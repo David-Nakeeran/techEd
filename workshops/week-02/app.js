4f6d5e9e35abe4cc09adb739ee463c33958c3751
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-05-07 16:16:44 +0100

    Updated

--- workshops/week-02/app.js
+++ workshops/week-02/app.js
@@ -1,48 +1,67 @@
 "use strict";
 
 // Callbacks
-const onAwesomeSuccess = (data) => {
-  console.log(`It was successful: ${data.message}`);
+// const onAwesomeSuccess = (data) => {
+//   console.log(`It was successful: ${data.message}`);
+// };
+
+// const onAwesomeFailure = () => {
+//   console.log("It failed :(");
+// };
+
+// // Coordinator
+// const myAwesomeFunction = (onSuccessCallback, onFailureCallback) => {
+//   console.log("Running myAwesomeFunction... doing complex tasks...");
+//   console.log("Complex task complete. I will notify the user");
+
+//   const success = Math.random() > 0.5;
+//   if (success) {
+//     onSuccessCallback({
+//       message: "This is the message of ultimate success",
+//     });
+//   } else {
+//     onFailureCallback();
+//   }
+// };
+
+// myAwesomeFunction(onAwesomeSuccess, onAwesomeFailure);
+
+// const add = (a, b) => {
+//   return a + b;
+// };
+
+// const multiply = (a, b) => {
+//   return a * b;
+// };
+
+// const myCalculatorFunction = (a, b, operation) => {
+//   console.log(
+//     `Magic is about to happen with these two numbers: ${a} and ${b} , are you ready?!`
+//   );
+//   return operation(a, b);
+// };
+
+// const result = myCalculatorFunction(3, 5, add);
+// console.log(result);
+
+// const result2 = myCalculatorFunction(2, 10, multiply);
+// console.log(result2);
+
+const person = {
+  firstName: "Bob",
+  surname: "Smith",
+  age: 39,
+  favouriteColour: "Green",
+  pets: ["Cat", "Hamster", "Monkey"],
+  getPersonFullName: function () {
+    return `${this.firstName} ${this.surname}`;
+  },
+  sayHello: function (message) {
+    return `${message} ${this.firstName}`;
+  },
 };
 
-const onAwesomeFailure = () => {
-  console.log("It failed :(");
-};
+const fullName = person.getPersonFullName();
+const greeting = person.sayHello("Hey, how's it going ");
 
-// Coordinator
-const myAwesomeFunction = (onSuccessCallback, onFailureCallback) => {
-  console.log("Running myAwesomeFunction... doing complex tasks...");
-  console.log("Complex task complete. I will notify the user");
-
-  const success = Math.random() > 0.5;
-  if (success) {
-    onSuccessCallback({
-      message: "This is the message of ultimate success",
-    });
-  } else {
-    onFailureCallback();
-  }
-};
-
-myAwesomeFunction(onAwesomeSuccess, onAwesomeFailure);
-
-const add = (a, b) => {
-  return a + b;
-};
-
-const multiply = (a, b) => {
-  return a * b;
-};
-
-const myCalculatorFunction = (a, b, operation) => {
-  console.log(
-    `Magic is about to happen with these two numbers: ${a} and ${b} , are you ready?!`
-  );
-  return operation(a, b);
-};
-
-const result = myCalculatorFunction(3, 5, add);
-console.log(result);
-
-const result2 = myCalculatorFunction(2, 10, multiply);
-console.log(result2);
+console.log(greeting);
