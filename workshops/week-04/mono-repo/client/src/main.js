3098120c1cf13e45026c6e61ef0e6655878e7a53
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-05-21 20:01:38 +0100

    updated

--- workshops/week-04/mono-repo/client/src/main.js
+++ workshops/week-04/mono-repo/client/src/main.js
@@ -1,24 +1,24 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+const form = document.querySelector("form");
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const submitHandler = async (e) => {
+  e.preventDefault();
 
-setupCounter(document.querySelector('#counter'))
+  const formData = new FormData(form);
+
+  const formValues = Object.fromEntries(formData);
+
+  const response = await fetch("http://localhost:8080/messages", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(formValues),
+  });
+
+  const result = await response.json();
+  if (result.success) {
+    form.reset();
+  }
+};
+
+form.addEventListener("submit", submitHandler);
