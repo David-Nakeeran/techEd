3350ead99c2e15ef77d42a8114e5a8f65c80bf80
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-06-10 18:27:26 +0100

    updated

--- workshops/week-06/react-playground/src/components/ShowMessageButton.jsx
+++ workshops/week-06/react-playground/src/components/ShowMessageButton.jsx
@@ -1,7 +1,7 @@
 export const ShowMessageButton = ({ messageClick }) => {
   return (
     <button
-      class="font-medium cursor-pointer bg-purple-400 text-purple-950 p-3 rounded-2xl hover:bg-purple-300"
+      className="font-medium cursor-pointer bg-purple-400 text-purple-950 p-3 rounded-2xl hover:bg-purple-300"
       onClick={() => messageClick()}
     >
       Click me to show message
